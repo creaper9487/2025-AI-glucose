a8df4ecca39cf3d729d6416acf1f203ff8643761
zou <leftrightch@gmail.com>
2024-10-18 11:59:43 +0800

    CORS

--- glucose-BE/glucoseBE/glucoseBE/settings.py
+++ glucose-BE/glucoseBE/glucoseBE/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -100,6 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
