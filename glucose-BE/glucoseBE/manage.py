3232fe7a80c9c9fbb1c40fa8c3cae61c4ddb42b5
zou <leftrightch@gmail.com>
2025-02-23 23:15:55 +0800

    ip print

--- glucose-BE/glucoseBE/manage.py
+++ glucose-BE/glucoseBE/manage.py
@@ -2,6 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import socket
+import time
+
+
+def get_local_ip():
+    try:
+        # 使用 UDP socket 連接到一個外部伺服器，但不會真的發送資料
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+        s.close()
+        return local_ip
+    except Exception as e:
+        return f"無法獲取 IP: {e}"
 
 
 def main():
@@ -15,6 +29,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    print("本機區域網 IP 地址:", get_local_ip())
     execute_from_command_line(sys.argv)
 
 
