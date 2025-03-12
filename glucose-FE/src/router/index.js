de55452ac56f55855caf36be12453b01c2dd58cb
zou <leftrightch@gmail.com>
2025-03-12 16:30:19 +0800

    feat: add global navigation guard for authentication and update routes with requiresAuth meta

--- glucose-FE/src/router/index.js
+++ glucose-FE/src/router/index.js
@@ -15,6 +15,7 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: HomeView,
+      meta: { requiresAuth: true } // 添加需要登入的標記
     },
     {
       path: '/figure',
@@ -23,6 +24,7 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/FigureView.vue'),
+      meta: { requiresAuth: true } // 添加需要登入的標記
     },
     {
       path: '/login',
